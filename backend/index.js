76ed26838d348ce1917fbad6bc41ab6e4f3a14f4
Daniel Souza <danieltidus@gmail.com>
2020-03-24 11:34:46 -0300

    chore(all components): testing husky
    Testing husky plugin

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
 
+const app = express();
+
+app.get('/users', (request, response) => response.json({
+  evento: 'Semana Omni Stack 11.0',
+  aluno: 'Daniel Faustino',
+}));
+
+app.listen(3333);
