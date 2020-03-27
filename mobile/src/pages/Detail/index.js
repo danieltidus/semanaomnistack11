fcf7709b3607c482a96ac51bedf4fae840657220
Daniel Souza <danieltidus@gmail.com>
2020-03-27 18:05:58 -0300

    feat(mobile): adding mobile app
    Adding mobile app with first screen

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
+import {Feather} from '@expo/vector-icons'
+import { useNavigation } from "@react-navigation/native";
+
+
+import logImg from '../../assets/logo.png'
+import styles from './styles'
+
+export default function Detail() {
+  return (
+    <View style={styles.container}/>
+  )
+}
